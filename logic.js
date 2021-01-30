0a051ed8908713706b69b3ee577b0c2e818eff45
kevinfrancetti <kevin.francetti91@gmail.com>
2021-01-31 00:31:17 +0100

    first commit

---
+++ logic.js
@@ -0,0 +1,16 @@
+const statusDisplay = document.querySelector(".game--status");
+
+let gameActive = true;
+
+let currentPlayer = "X";
+
+function newContent() {
+    //document.open();
+    document.write("<h1>Out with the old, in with the new!</h1>");
+    //document.close();
+}
+
+
+function spamAllert(){
+    window.alert("DIO CAN");
+}
