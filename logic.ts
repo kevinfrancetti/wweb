1af89796715480678db6d226f3cefbfc7f5f092b
kevinfrancetti <kevin.francetti91@gmail.com>
2021-02-11 02:41:38 +0100

    bro

--- logic.ts
+++ logic.ts
@@ -1,25 +1,36 @@
-import {sayHi} from './sayhi.js';
-import {Hub} from './tree.js';
+import { sayHi } from './sayhi.js';
+import { Hub } from './tree.js';
 
 //Debug proupose
 console.log('type for import stuff for debugging: import(\'/scripts/logic.js\').then( m => module = m); ');
 
-const statusDisplay: Element = document.querySelector(".game--status");
-const cellList: NodeListOf<Element> = document.querySelectorAll("button, .cell");
 
-let gameSize: number = 3;
-let gameMemory = [];
+//###GAME SETUP
+const cellList: NodeListOf<Element> = document.querySelectorAll(".cell");
+const buttonList: NodeListOf<Element> = document.querySelectorAll("button");
+const gameSize: number = 3;
+const gameMemory = [];
 
-for(let i = 0; i < gameSize; i++){
+for (let i = 0; i < gameSize; i++) {
     gameMemory[i] = [];
-    for(let j = 0; j < gameSize; j++){
+    for (let j = 0; j < gameSize; j++) {
         gameMemory[i][j] = undefined;
     }
 }
 
-for (let i = 0; i < cellList.length; i++) {
-    cellList[i].addEventListener('click', handleCellClick);
-}
+(() => {
+    for (let i = 0; i < cellList.length; i++) {
+        cellList[i].setAttribute('data-x', `${i%gameSize}`);
+        cellList[i].setAttribute('data-y', `${Math.trunc(i/gameSize)}`);
+        cellList[i].addEventListener('click', handleCellClick);
+        gameMemory[i%gameSize][Math.trunc(i/gameSize)] = cellList [i];
+    }
+})();
+buttonList.forEach(btn => btn.addEventListener('click', handleCellClick));
+//###GAME SETUP
+
+
+const statusDisplay: Element = document.querySelector(".game--status");
 
 function handleCellClick(event: Event) {
     let target: EventTarget = event.target;
@@ -38,21 +49,10 @@ function handleCellClick(event: Event) {
     console.log(((target as HTMLDivElement).getAttribute('data-cell-index')));
 }
 
-let gameActive = true;
-let currentPlayer = "X";
-
 function newContent() {
     //document.open();
     document.write("<h1>Out with the old aids, in with the new!</h1>");
     //document.close();
 }
 
-function spamAllert() {
-    window.alert("DIO CAN");
-}
-
-function Yolo() {
-    console.log("From Yeolo", this);
-}
-
-export {spamAllert as culo, Hub, gameMemory};
+export {Hub, gameMemory};
