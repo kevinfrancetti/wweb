d24cd33feac2f07a1a155365358f5ee7e679e94d
kevinfrancetti <kevin.francetti91@gmail.com>
2021-02-16 16:00:31 +0100

    preparing for branch

--- logic.ts
+++ logic.ts
@@ -7,14 +7,15 @@ console.log('type for import stuff for debugging: import(\'/scripts/logic.js\').
 
 //###GAME SETUP
 const cells: NodeListOf<Element> = document.querySelectorAll(".cell");
-const buttonList: NodeListOf<Element> = document.querySelectorAll("button");
+const buttons: NodeListOf<Element> = document.querySelectorAll("button");
 const gameSize: number = 3;
 enum GameTurn {
     X = 'X',
     O = 'O'
 }
+let turn = GameTurn.X;
 //#GAME STATE
-const gameMemory: object[] = [];//Memory of the game AKA game state
+const gameMemory: object[] = [];//Memory of the game AKA game state [column][rows]
 let currentTurn: string = GameTurn.X;
 
 for (let i = 0; i < gameSize; i++) {
@@ -33,7 +34,7 @@ for (let i = 0; i < cells.length; i++) {
     //cellList[i].innerHTML = '';
     gameMemory[memPosition.x][memPosition.y] = cells[i];
 }
-buttonList.forEach(btn => btn.addEventListener('click', handleCellClick));
+buttons.forEach(btn => btn.addEventListener('click', handleCellClick));
 
 
 //###UTIL FUNCTIONS
@@ -47,15 +48,36 @@ function mapListToSquareMatrix(listIndex: number, matrixSize: number) {
 }
 
 function checkWin(matrix: object[], label: string) {
-
+    //Check columns
     for (let i = 0; i < matrix.length; i++) {
-        let mem = matrix[i][0].innerText;
-        if (mem == '') continue;//if first square is void no point furter check
-
+        let tmp = matrix[i][0].innerText;
+        if (tmp == '') continue;//if first square is void no point furter check
         for (let j = 1; j < matrix.length; j++) {
-            if (matrix[i][j].innerText != mem) break;
-            if (j == matrix.length - 1) console.log('win');
+            if (matrix[i][j].innerText != tmp) break;
+            if (j == matrix.length - 1) console.log(`${tmp} wins`);
         }
+    }
+    //Check rows
+    for (let i = 0; i < matrix.length; i++) {
+        let tmp = matrix[0][i].innerText;
+        if (tmp == '') continue;//if first square is void no point furter check
+        for (let j = 1; j < matrix.length; j++) {
+            if (matrix[j][i].innerText != tmp) break;
+            if (j == matrix.length - 1) console.log(`${tmp} wins`);
+        }
+    }
+    //Check diagonals
+    let tmp = matrix[0][0].innerText;
+    for(let i = 1; i < matrix.length; i++){
+        if(tmp == '') break;
+        if(matrix[i][i].innerText != tmp) break;
+        if (i == matrix.length - 1) console.log(`${tmp} wins`);
+    }
+    tmp = matrix[0][matrix.length - 1].innerText;
+    for(let i = 1; i < matrix.length; i++){
+        if(tmp == '') break;
+        if(matrix[i][matrix.length -1 - i].innerText != tmp) break;
+        if (i == matrix.length - 1) console.log(`${tmp} wins`);
     }
 }
 
@@ -68,12 +90,17 @@ function handleCellClick(event: Event) {
     let y: number = parseInt(target.getAttribute('data-y'));
     let cellText: string = target.innerText;
 
+
+    if (turn == GameTurn.X) {
+        turn = GameTurn.O;
+    } else turn = GameTurn.X;
+
     //###STAGE_2 UPDATE STATE
-    checkWin(gameMemory, GameTurn.X);
+    checkWin(gameMemory, turn);
     //RENDER
     if (target.constructor.name == HTMLDivElement.name) {
         console.log("This is a HTMLDivElement");
-        (target as HTMLDivElement).innerText = GameTurn.X;
+        (target as HTMLDivElement).innerText = turn;
     } else if (target.constructor.name == HTMLButtonElement.name) {
         console.log("This is a HTMLButtonElement");
     }
